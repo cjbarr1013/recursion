278587c768793406cb84ee3379a5d6da08c18284
Cameron Barr <cjbarr1013@gmail.com>
2024-12-03 13:19:19 -0500

    Create function to merge sort a given array of #s

--- script.js
+++ script.js
@@ -40,3 +40,28 @@ console.log(fibsRec(0));
 console.log(fibsRec(1));
 console.log(fibsRec(2));
 console.log(fibsRec(8));
+
+// Merge Sort
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+
+  let arrFinal = [];
+  let arr1 = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+  let arr2 = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+
+  while (arr1.length > 0 || arr2.length > 0) {
+    if (arr1[0] <= arr2[0] || arr2.length === 0) {
+      arrFinal.push(arr1.shift());
+    } else {
+      arrFinal.push(arr2.shift());
+    }
+  }
+
+  return arrFinal;
+}
+
+let arr = [
+  3, 55, 4, 18, 5, 0, 2, 1, 13, 8, 5, 0, 1, 2, 33, 2, 5, 4, 6, 3, 7, 0, 4, 23,
+  55, 5, 8, 9, 77, 4, 0, 2, 4, 8, 9, 22, 21, 34,
+];
+console.log(mergeSort(arr));
